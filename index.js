3c2f30ed8c7a6e3bf9ff68cd0f8d1b554c74b7f9
sinvan <muhammedsinvan3@gmail.com>
2023-07-27 06:27:34 +0530

    router setup

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import router from "./routers/routes.js";
 
 const app = express();
 dotenv.config();
 
 app.use(express.json({limit:'25mb'}));
+
+app.use('/',router)
 
 const PORT = process.env.PORT || 3000;
 
